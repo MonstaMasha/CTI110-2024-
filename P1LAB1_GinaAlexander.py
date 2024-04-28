5d10c2f45a34afef81577722698e6335324125a3
MonstaMasha <126363795+MonstaMasha@users.noreply.github.com>
2024-04-28 14:29:42 -0400

    Add files via upload

---
+++ P1LAB1_GinaAlexander.py
@@ -0,0 +1,17 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Alexander Gina
+... 
+... #4/28/2024
+... 
+... #P1LAB1
+... 
+... #This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.
+>>> user_name = input()
+Pat
+>>> 
+>>> print ('Hey',user_name)
+Hey Pat
+>>> 
+>>> print ('Welcome to zyBooks!')
+Welcome to zyBooks!
