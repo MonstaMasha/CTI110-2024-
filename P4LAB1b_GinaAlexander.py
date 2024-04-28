b76a7dc08b7d27bb73ed4a9bb0122f29a49c569b
MonstaMasha <126363795+MonstaMasha@users.noreply.github.com>
2024-04-28 14:59:27 -0400

    Add files via upload

---
+++ P4LAB1b_GinaAlexander.py
@@ -0,0 +1,29 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> wn = turtle.Screen()
+>>> wn.bgcolor("crimson")
+>>> wn.title("Initials")
+>>> 
+>>> alex = turtle.Turtle()
+>>> alex.color("gold")
+>>> alex.pensize(5)
+>>> 
+>>> gina = turtle.Turtle()
+>>> gina.color("steelblue")
+>>> gina.pensize(5)
+>>> 
+>>> alex.backward(150)
+>>> alex.forward(130)
+>>> alex.right(45)
+>>> alex.forward(50)
+>>> alex.backward(150)
+>>> 
+>>> alex.right(90)
+>>> alex.forward(150)
+>>> 
+>>> gina.circle(-80, -180)
+>>> gina.right(90)
+>>> gina.forward(50)
+>>> gina.right(90)
+>>> gina.backward(50)
